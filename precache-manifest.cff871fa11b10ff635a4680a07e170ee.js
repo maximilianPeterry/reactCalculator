0968e2052c36850fc8a8a54c4468febbad106c11
CN0107 <cn0107@CN0107.lan>
2019-06-19 16:16:33 +0100

    Updates

---
+++ precache-manifest.cff871fa11b10ff635a4680a07e170ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9faeb08ff0556f091abbf859b473d718",
+    "url": "/reactCalculator/index.html"
+  },
+  {
+    "revision": "adbf3adbf15d118ba610",
+    "url": "/reactCalculator/static/css/main.2f074671.chunk.css"
+  },
+  {
+    "revision": "efc3d230fd1e1b3caa34",
+    "url": "/reactCalculator/static/js/2.bbcd3f81.chunk.js"
+  },
+  {
+    "revision": "adbf3adbf15d118ba610",
+    "url": "/reactCalculator/static/js/main.10acb404.chunk.js"
+  },
+  {
+    "revision": "53770aee77960065190d",
+    "url": "/reactCalculator/static/js/runtime~main.aac63372.js"
+  }
+]);
